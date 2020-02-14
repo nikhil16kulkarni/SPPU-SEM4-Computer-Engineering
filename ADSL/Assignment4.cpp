3b51d510c983337a10a92ce65c7df75195123216
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2020-02-14 14:16:41 +0530

    Assignment4 Completed

--- ADSL/Assignment4.cpp
+++ ADSL/Assignment4.cpp
@@ -6,7 +6,506 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+
+
 #include <iostream>
+#include<string.h>
+using namespace std;
+class CityNode;
+class Queue
+{
+	char *data[100];
+	int front,rear;
+public:
+	Queue()
+	{
+		front=rear=-1;
+	}
+	bool empty()
+	{
+		if(rear==-1)
+			return true;
+		return false;
+	}
+	bool full()
+	{
+		if(front==((rear+1)%100))
+			return true;
+		return false;
+	}
+	void push(char *p)
+	{
+		if(!full())
+		{
+			if(empty())
+				front=0;
+			rear=(rear+1)%100;
+			data[rear]=p;
+		}
+	}
+	char* pop()
+	{
+		if(!empty())
+		{
+			char*p=data[front];
+			if(front==rear)
+				front=rear=-1;
+			else
+				front=(front+1)%100;
+			return p;
+		}
+		else
+			return NULL;
+	}
+};
+class Node
+{
+	char *city;
+	int time;
+	Node *next;
+	friend class CityNode;
+	friend class Flight;
+public:
+	Node()
+	{
+		next=NULL;
+		time=-1;
+		city=NULL;
+	}
+	Node(char *s,int t)
+	{
+		time=t;
+		city=new char[strlen(s)+1];
+		next=NULL;
+		strcpy(city,s);
+	}
+	~Node()
+	{
+		delete city;
+	}
+};
+class CityNode
+{
+	CityNode *down;
+	Node *head;
+	char *cname;
+	friend class Flight;
+public:
+	CityNode()
+	{
+		down=NULL;
+		head=NULL;
+		cname=NULL;
+	}
+	CityNode(char *s)
+	{
+		cname=new char[strlen(s)+1];
+		down=NULL;
+		head=NULL;
+		strcpy(cname,s);
+	}
+	~CityNode()
+	{
+		Node *p=head,*q;
+		while(p!=NULL)
+		{
+			q=p;
+			p=p->next;
+			delete q;
+		}
+		head=NULL;
+		delete cname;
+	}
+	void display();
+};
+void CityNode::display()
+{
+	Node *p=head;
+	while(p->next!=NULL)
+	{
+		cout<<p->city<<"("<<p->time<<"),";
+		p=p->next;
+	}
+	cout<<p->city<<"("<<p->time<<")\n";
+}
+class Flight
+{
+	CityNode *start;
+public:
+	Flight()
+	{
+		start=NULL;
+	}
+	CityNode* get_pos(char*);
+	int get_index(char*);
+	void set(int *&,int&);
+	void create();
+	void disp_city();
+	void add_city(char*);
+	void add_path(char*,char*,int);
+	void disp_path();
+	bool search_path(char*,char*);
+	bool search_city(char*);
+	void remove_path(char*,char*);
+	void remove_city(char*);
+	void degree(char*);
+	void DFS_rec();
+	void DFS(char*,int*);
+	void BFS();
+};
+void Flight::BFS()
+{
+	Queue Q;
+	int *visited,n;
+	set(visited,n);
+	char src[50];
+	cout<<"Enter the strating vertex\n";
+	cin.getline(src,48);
+	cout<<"BFS Traversal\n";
+	CityNode*p;
+	Node*q;
+	Q.push(src);
+	while(!Q.empty())
+	{
+		strcpy(src,Q.pop());
+		if(!visited[get_index(src)])
+			cout<<src<<"\n";
+		visited[get_index(src)]=1;
+		p=get_pos(src);
+		q=p->head;
+		while(q!=NULL)
+		{
+			if(!visited[get_index(q->city)])
+				Q.push(q->city);
+			q=q->next;
+		}
+	}
+}
+void Flight::DFS_rec()
+{
+	int *visited,n;
+	char src[50];
+	set(visited,n);
+	cout<<"Enter starting vertex\n";
+	cin.getline(src,48);
+	cout<<"DFS Traversal is\n";
+	DFS(src,visited);
+}
+void Flight::DFS(char *src,int visited[])
+{
+	cout<<src<<"\n";
+	visited[get_index(src)]=1;
+	CityNode*p=get_pos(src);
+	Node*q=p->head;
+	while(q!=NULL)
+	{
+		if(!visited[get_index(q->city)])
+			DFS(q->city,visited);
+		q=q->next;
+	}
+}
+void Flight::set(int *&p,int &n)
+{
+	n=0;
+	CityNode*q=start;
+	while(q!=NULL)
+	{
+		n++;
+		q=q->down;
+	}
+	p=new int[n];
+	for(int i=0;i<n;i++)
+		p[i]=0;
+}
+int Flight::get_index(char *s)
+{
+	int i=0;
+	CityNode *p=start;
+	while(strcmp(p->cname,s))
+	{
+		i++;
+		p=p->down;
+	}
+	return i;
+}
+bool Flight::search_city(char *c)
+{
+	CityNode*p=start;
+	while(p!=NULL)
+	{
+		if(strcmp(c,p->cname)==0)
+			return true;
+		p=p->down;
+	}
+	return false;
+}
+bool Flight::search_path(char s[],char d[])
+{
+	CityNode *p=get_pos(s);
+	Node *q=p->head;
+	while(q!=NULL)
+	{
+		if(strcmp(q->city,d)==0)
+			return true;
+		q=q->next;
+	}
+	return false;
+}
+CityNode* Flight::get_pos(char *s)
+{
+	CityNode*p=start;
+	while(p!=NULL)
+	{
+		if(strcmp(p->cname,s)==0)
+			return p;
+		p=p->down;
+	}
+	return NULL;
+}
+void Flight::create()
+{
+	CityNode *p=start;
+	char str[100];
+	while(true)
+	{
+		cout<<"Enter City names or STOP\n";
+		cin.getline(str,99);
+		if(strcmp(str,"STOP")==0)
+			break;
+		if(p==NULL)
+		{
+			start=new CityNode(str);
+			p=start;
+		}
+		else
+		{
+			p=start;
+			while(p->down!=NULL)
+				p=p->down;
+			p->down=new CityNode(str);
+		}
+	}
+}
+void Flight::disp_city()
+{
+	CityNode *p=start;
+	if(p==NULL)
+	{
+		cout<<"No cities present, add city first\n";
+		return;
+	}
+	cout<<"----------------------------------------\n";
+	cout<<"List of cities: ";
+	while(p->down!=NULL)
+	{
+		cout<<p->cname<<", ";
+		p=p->down;
+	}
+	cout<<p->cname<<"\n";
+	cout<<"----------------------------------------\n";
+}
+void Flight::add_city(char s[])
+{
+	CityNode*p=start;
+	while(p->down!=NULL)
+		p=p->down;
+	p->down=new CityNode(s);
+}
+void Flight::add_path(char s[],char d[],int t)
+{
+	CityNode *p=get_pos(s);
+	Node *q=p->head;
+	if(q==NULL)
+	{
+		p->head=new Node(d,t);
+		return;
+	}
+	while(q->next!=NULL)
+	{
+		q=q->next;
+	}
+	q->next=new Node(d,t);
+}
+void Flight::disp_path()
+{
+	cout<<"----------------------------------------\n";
+	cout<<"ALL the paths are\n";
+	CityNode*p=start;
+	while(p!=NULL)
+	{
+		if(p->head!=NULL)
+		{
+			cout<<p->cname<<" To: ";
+			p->display();
+		}
+		p=p->down;
+	}
+	cout<<"----------------------------------------\n";
+}
+void Flight::remove_path(char s[],char d[])
+{
+	CityNode*p=get_pos(s);
+	Node*q=p->head,*r=NULL;
+	while(strcmp(q->city,d)!=0)
+	{
+		r=q;
+		q=q->next;
+	}
+	if(q==p->head)
+	{
+		p->head=(p->head)->next;
+		delete q;
+	}
+	else
+	{
+		r->next=q->next;
+		delete q;
+	}
+}
+void Flight::remove_city(char ct[])
+{
+	CityNode*p=get_pos(ct),*q=start;
+	while(q!=NULL)
+	{
+		if(q!=p)
+		{
+			if(search_path(q->cname,ct))
+				remove_path(q->cname,ct);
+		}
+		q=q->down;
+	}
+	q=start;
+	while(q->down!=p)
+		q=q->down;
+	if(p==start)
+		start=start->down;
+	else
+		q->down=p->down;
+	delete p;
+}
+void Flight::degree(char ct[])
+{
+	int in=0,out=0;
+	CityNode*p=get_pos(ct);
+	Node*q=p->head;
+	while(q!=NULL)
+	{
+		out++;
+		q=q->next;
+	}
+	CityNode*r=start;
+	while(r!=NULL)
+	{
+		if(r!=p)
+		{
+			q=r->head;
+			while(q!=NULL)
+			{
+				if(strcmp(q->city,ct)==0)
+					in++;
+				q=q->next;
+			}
+		}
+		r=r->down;
+	}
+	cout<<"In-degree is "<<in<<"\n";
+	cout<<"Out-degree is "<<out<<"\n";
+}
+int main()
+{
+	int ch,t;
+	char str[100],str2[100],c[100];
+	Flight F1;
+	cout<<"Add some cities first\n";
+	F1.create();
+	F1.disp_city();
+	do
+	{
+		cout<<"Enter\n1 To Add City\n2 To Add Path\n3 To Remove Path\n4 To Remove City";
+		cout<<"\n5 To Calculate In-degree and Out-degree\n6 For Traversals\n0 To EXIT\n";
+		cin>>ch;
+		cin.ignore();
+		switch(ch)
+		{
+		case 1:
+			cout<<"Enter City name to add\n";
+			cin.getline(c,99);
+			if(!F1.search_city(c))
+				F1.add_city(c);
+			else
+				cout<<"City Already Present\n";
+			F1.disp_city();
+			break;
+		case 2:
+			cout<<"Enter Source City\n";
+			cin.getline(str,99);
+			cout<<"Enter Destination City\n";
+			cin.getline(str2,99);
+			cout<<"Enter Time\n";
+			cin>>t;
+			cin.ignore();
+			if(F1.search_city(str)&&F1.search_city(str2))
+			{
+				if(!F1.search_path(str,str2))
+					F1.add_path(str,str2,t);
+				else
+					cout<<"Such path already exists\n";
+			}
+			else
+				cout<<"Reference City name not present in the list\n";
+			F1.disp_path();
+			break;
+		case 3:
+			cout<<"Enter Source City\n";
+			cin.getline(str,99);
+			cout<<"Enter Destination City\n";
+			cin.getline(str2,99);
+			if(F1.search_city(str)&&F1.search_city(str2))
+			{
+				if(F1.search_path(str,str2))
+					F1.remove_path(str,str2);
+				else
+					cout<<"No such path exists\n";
+			}
+			else
+				cout<<"Reference city name not present\n";
+			F1.disp_path();
+			break;
+		case 4:
+			cout<<"Enter City Name\n";
+			cin.getline(str,99);
+			if(F1.search_city(str))
+				F1.remove_city(str);
+			else
+				cout<<"Reference city name not present\n";
+			F1.disp_city();
+			F1.disp_path();
+			break;
+		case 5:
+			cout<<"Enter City Name\n";
+			cin.getline(str,99);
+			if(F1.search_city(str))
+				F1.degree(str);
+			else
+				cout<<"Reference city name not present\n";
+			break;
+		case 6:
+			cout<<"TRAVERSALS\n";
+			F1.DFS_rec();
+			F1.BFS();
+			break;
+		case 0:
+			cout<<"You have opted to EXIT\n";
+			break;
+		default:
+			cout<<"Invalid Choice\n";
+		}
+	}while(ch);
+	return 0;
+}
+
+
+/*#include <iostream>
 #include<string.h>
 #define max 10
 using namespace std;
@@ -418,7 +917,7 @@ int main() {
 }
 
 
-/*class Gnode{
+class Gnode{
 	string dest;
 	int cost;
 	Gnode *next;
@@ -524,7 +1023,7 @@ void graph::create(){
 	}
 }
 
-/*void graph::delete(){
+void graph::delete(){
 
 }*/
 
