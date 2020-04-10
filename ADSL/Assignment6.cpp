afa5587ad00d0087d699c312aef81b7e63f77bfe
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2020-04-10 23:36:24 +0530

    Update Assignment6.cpp

--- ADSL/Assignment6.cpp
+++ ADSL/Assignment6.cpp
@@ -231,7 +231,7 @@ int main() {
 
 	return 0;
 } */
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -397,3 +397,311 @@ int main() {
 	while(n!=4);
 	return 0;
 }
+*/
+
+//============================================================================
+// Name        : Assignment6.cpp
+// Author      : Nikhil Kulkarni
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+#define max 26
+
+class Entry
+{
+	string key,value;
+public:
+	Entry()
+	{
+		key.clear();
+		value.clear();
+	}
+	Entry(string key,string value)
+	{
+		this->key=key;
+		this->value=value;
+	}
+	friend class Hashtable;
+};
+class Hashtable
+{
+public:
+	Entry e[max];
+	int n;
+	Hashtable()
+	{
+		n=0;
+	}
+	int hashfun(string key)
+	{
+		int x=key[0],i;
+		if(x<=90&&x>=65)
+		{
+			i=x-65;
+		}
+		else if(x<=122 && x>=97)
+		{
+			i=x-97;
+		}
+		return i;
+	}
+	void display()
+	{
+		cout<<"     "<<"key"<<"   "<<"value\n";
+		for(int i=0;i<26;i++)
+		{
+			if(i>9)
+			{
+			if(e[i].key!="\0"&&e[i].value!="\0")
+			{
+				cout<<i<<"   "<<e[i].key<<"   "<<e[i].value<<endl;
+			}
+			else
+			{
+				cout<<i<<"   "<<"---"<<"   "<<"---"<<endl;
+			}
+			}
+			else
+			{
+				if(e[i].key!="\0"&&e[i].value!="\0")
+						{
+							cout<<"0"<<i<<"   "<<e[i].key<<"   "<<e[i].value<<endl;
+						}
+						else
+						{
+							cout<<"0"<<i<<"   "<<"---"<<"   "<<"---"<<endl;
+						}
+			}
+		}
+	}
+	void modify()
+	{
+	string word,meaning,w;
+		int j=search();
+		if(j==-1)
+		{
+			return ;
+		}
+		else
+		{
+			cout<<"ENTER MEANING :-";
+			cin>>meaning;
+			e[j].value=meaning;
+		}
+	}
+
+	void insert_without()
+	{
+		string key,meaning;
+		int i;
+		while(1)
+		{
+			cout<<"Enter word to insert";
+			cin>>key;
+			if(key=="-1")
+			{
+				break;
+			}
+			if(n>=26)
+			{
+				cout<<"HASH TABLE IS FULL\n"<<endl;
+				break;
+			}
+			cout<<"Enter meaning to insert";
+			cin>>meaning;
+			n++;
+			i=hashfun(key);
+			int j=i;
+
+				while(e[j].key!="\0")
+				{
+					j++;
+					if(j>25&&n<26)
+					{
+						j=j%26;
+					}
+				}
+				e[j].key=key;
+				e[j].value=meaning;
+
+
+		}
+		display();
+	}
+	int search()
+	{
+		int i,j,count=0;
+		string w;
+		cout<<"Enter string";
+		cin>>w;
+		i=hashfun(w);
+		j=i;
+		while(1)
+		{
+			if(e[j].key==w)
+			{
+				cout<<"KEY FOUND\n";
+				cout<<"value: "<<e[j].key<<" meaning: "<<e[j].value<<endl;
+				return j;
+			}
+			j++;
+			count++;
+			if(j>25)
+			{
+				j=j%26;
+			}
+			if(j==i)
+			{
+				cout<<"KEY NOT FOUND\n";
+				return -1;
+			}
+		}
+	}
+	void insert_with()
+	{
+		string w,m;
+		int i;
+		while(1)
+		{
+			cout<<"Enter word to insert";
+			cin>>w;
+			if(w=="-1")
+			{
+				return;
+			}
+			if(n>=26)
+			{
+				cout<<"HASH TABLE IS FULL\n";
+				break;
+			}
+			cout<<"INSERT MEANING: ";
+			cin>>m;
+			n++;
+			i=hashfun(w);
+			int j=i;
+			if(e[j].key!="\0"&&e[j].key[0]!=w[0])
+			{
+				string q,r;
+				q=e[j].key;
+				r=e[j].value;
+				e[j].key=w;
+				e[j].value=m;
+				while(e[j].key!="\0")
+				{
+					j++;
+					if(j>=26)
+					{
+						j=j%26;
+					}
+				}
+				e[j].key=q;
+				e[j].value=r;
+			}
+			else
+			{
+				while(e[j].key!="\0")
+				{
+					j++;
+					if(j>=26)
+					{
+						j=j%26;
+					}
+				}
+				e[j].key=w;
+				e[j].value=m;
+			}
+
+		}
+
+	}
+	void delete_()
+    {
+        string w;
+        Entry t;
+        while(1)
+        {
+        cout<<"Enter string to delete: ";
+        cin>>w;
+        if(w=="-1")
+        {
+        break;
+        }
+        int i=hashfun(w);
+        int j=i;
+        while(1)
+        {
+            if(e[j].key==w)
+            {
+                cout<<"Word deleted:"<<endl;
+               // cout<<e[j].key<<" = "<<e[j].value<<endl;
+                for(int k=j+1;k<26;k++)
+                {
+                if(e[k].key[0]==w[0])
+                {
+                e[j] = e[k];
+                j = k;
+                }
+                }
+                e[j].value="\0";
+                e[j].key="\0";
+                for(int i=0;i<26;i++)
+                {
+                if(hashfun(e[i].key)==j&&e[j].key=="\0")
+                {
+                t = e[j];
+                e[j] = e[i];
+                e[i] = t;
+                }
+                }
+                break;
+            }
+            if(j==i+26)
+            {
+                cout<<"Word not found..."<<endl;
+                break;
+            }
+            j++;
+        }
+        }
+
+    }
+};
+int main()
+{
+	int ch;
+	Hashtable h;
+
+	do{
+	cout<<"1.CREATE WITH REPLACEMENT \n2.CREATE WITHOUT REPLACEMENT\n3.SEARCH\n4.MODIFY\n5.DELETE ENTRY\n6.EXIT\n";
+	cin>>ch;
+	switch(ch)
+	{
+	case 1:
+		h.insert_with();
+		h.display();
+		break;
+	case 2:
+		h.insert_without();
+		h.display();
+		break;
+	case 3:
+		h.search();
+		break;
+	case 4:
+		h.modify();
+		h.display();
+		break;
+	case 5:
+		h.delete_();
+		h.display();
+		break;
+	}
+	}while(ch!=6);
+
+
+}
+
