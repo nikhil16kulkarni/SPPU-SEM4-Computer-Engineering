e1d3b0e3b7f5b2d5192bd15de5f778d221db6647
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2020-04-10 23:40:01 +0530

    Update Assignment7.cpp

--- ADSL/Assignment7.cpp
+++ ADSL/Assignment7.cpp
@@ -1,3 +1,5 @@
+/*
+
 //============================================================================
 // Name        : Assignment7.cpp
 // Author      : Nikhil Kulkarni
@@ -353,3 +355,445 @@ int main()
 
 
  */
+
+//============================================================================
+// Name        : Assignment7.cpp
+// Author      : Nikhil Kulkarni
+// Version     :
+// Copyright  : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include<iostream>
+#include<string>
+using namespace std;
+class Symbol
+{
+	public:
+	string identef;
+	int val;
+	friend class HashTable;
+};
+class HashTable
+{
+	Symbol S[50];
+	int chain[50];
+	public:
+	HashTable()
+	{
+		for(int i=0;i<50;i++)
+		{
+			S[i].identef="\0";
+			S[i].val=chain[i]=-1;
+		}
+	}
+	int hashFunc(string);
+	void disp();
+	void printInfo(int);
+	void insertWithReplacement(string,int);
+	int searchWithReplacement(string,int&);
+	void modifyWithReplacement(string,int);
+	void delWithReplacement(string);
+	void insertWoReplacement(string,int);
+	int searchWoReplacement(string,int&);
+	void modifyWoReplacement(string,int);
+	void delWoReplacement(string);
+	void upAdjust(int);
+};
+void HashTable::printInfo(int i)
+{
+	cout<<"Idx\t\tIdent\t\tVal\n";
+	cout<<i<<"\t\t"<<S[i].identef<<"\t\t"<<S[i].val<<"\n";
+}
+void HashTable::disp()
+{
+	cout<<"Idx\t\tIdent\t\tVal\t\tChain\n";
+	for(int i=0;i<50;i++)
+	{
+		if(S[i].identef!="\0")
+			cout<<i<<"\t\t"<<S[i].identef<<"\t\t"<<S[i].val<<"\t\t"<<chain[i]<<"\n";
+	}
+}
+int HashTable::hashFunc(string str)
+{
+	int i=0,ans=0;
+	while(str[i]!='\0')
+	{
+		ans=ans+(int)str[i];
+		i++;
+	}
+	return ans%50;
+}
+void HashTable::insertWithReplacement(string idnt,int v)
+{
+	int idx=hashFunc(idnt);
+	if(S[idx].identef=="\0")
+	{
+		S[idx].identef=idnt;
+		S[idx].val=v;
+		return;
+	}
+	int temp=hashFunc(S[idx].identef);
+	if(temp==idx)
+	{
+		int c=idx,i=idx+1;
+		while(S[i].identef!="\0")
+			i=(i+1)%50;
+		while(chain[c]!=-1)
+			c=chain[c];
+		S[i].identef=idnt;
+		S[i].val=v;
+		chain[c]=i;
+	}
+	else
+	{
+		int i=idx+1;
+		while(S[i].identef!="\0")
+			i=(i+1)%50;
+		int c=temp;
+		while(chain[c]!=idx)
+			c=chain[c];
+		chain[c]=chain[idx];
+		while(chain[c]!=-1)
+			c=chain[c];
+		S[i]=S[idx];
+		chain[c]=i;
+		S[idx].identef=idnt;
+		S[idx].val=v;
+		chain[idx]=-1;
+	}
+}
+int HashTable::searchWithReplacement(string str,int &col)
+{
+	int idx=hashFunc(str);
+	if(S[idx].identef==str)
+	{
+		col=0;
+		return idx;
+	}
+	if(hashFunc(S[idx].identef)==idx)
+	{
+		col=1;
+		int c=idx;
+		while(chain[c]!=-1)
+		{
+			c=chain[c];
+			if(S[c].identef==str)
+				return c;
+			else
+				col++;
+		}
+		col=-1;
+		return -1;
+	}
+	else//Ele not present
+	{
+		col=-1;
+		return -1;
+	}
+}
+void HashTable::modifyWithReplacement(string str,int tar)
+{
+	int col=0,idx=searchWithReplacement(str,col);
+	if(idx==-1)
+	{
+		cout<<"Identifier Not Present\n";
+		return;
+	}
+	S[idx].val=tar;
+}
+void HashTable::delWithReplacement(string str)
+{
+	int col=0,idx=searchWithReplacement(str,col);
+	if(idx==-1)
+	{
+		cout<<"Identifier Not Present\n";
+		return;
+	}
+	int i=hashFunc(str);
+	if(i==idx)//First Ele in Chain
+	{
+		if(chain[i]==-1)
+		{
+			S[i].identef="\0";
+			S[i].val=-1;
+			return;
+		}
+		int c=chain[i];
+		S[i]=S[c];
+		chain[i]=chain[c];
+		S[c].identef="\0";
+		S[c].val=-1;
+		return;
+	}
+	int c=i;
+	while(chain[c]!=idx)
+		c=chain[c];
+	chain[c]=chain[idx];
+	S[idx].identef="\0";
+	S[idx].val=-1;
+}
+void HashTable::insertWoReplacement(string str,int val)
+{
+	int idx=hashFunc(str);
+	if(S[idx].identef=="\0")
+	{
+		S[idx].identef=str;
+		S[idx].val=val;
+		return;
+	}
+	int i=(idx+1)%50;
+	bool flag=true;
+	while(S[i].identef!="\0")
+		i=(i+1)%50;
+	int c=idx;
+	while(hashFunc(S[c].identef)!=idx)
+	{
+		c=(c+1)%50;
+		if(c==idx)
+		{
+			break;
+			flag=false;
+		}
+	}
+	while(chain[c]!=-1)
+		c=chain[c];
+	if(flag&&hashFunc(S[c].identef)==idx)
+		chain[c]=i;
+	S[i].identef=str;
+	S[i].val=val;
+}
+int HashTable::searchWoReplacement(string str,int &col)
+{
+	int idx=hashFunc(str);
+	if(S[idx].identef==str)
+	{
+		col=0;
+		return idx;
+	}
+	if(idx==hashFunc(S[idx].identef))
+	{
+		int c=chain[idx];
+		col=1;
+		while(c!=-1)
+		{
+			if(S[c].identef==str)
+				return c;
+			else
+				col++;
+			c=chain[c];
+		}
+		return -1;
+	}
+	int i=idx+1;
+	col=1;
+	while(hashFunc(S[i].identef)!=idx&&i!=idx)
+	{
+		col++;
+		i=(i+1)%50;
+	}
+	if(i==idx)
+		return -1;
+	if(S[i].identef==str)
+		return i;
+	int c=chain[i];
+	while(c!=-1&&S[c].identef!=str)
+		c=chain[c];
+	return c;
+}
+void HashTable::modifyWoReplacement(string str,int v)
+{
+	int col=0,idx=searchWoReplacement(str,col);
+	if(idx==-1)
+	{
+		cout<<"Identifier Not Present\n";
+		return;
+	}
+	S[idx].val=v;
+}
+void HashTable::delWoReplacement(string str)
+{
+	int col=0,idx=searchWoReplacement(str,col);
+	if(idx==-1)
+	{
+		cout<<"Identifier Not Present\n";
+		return;
+	}
+	int i=hashFunc(str),pos=0;
+	while(S[pos].identef=="\0")
+		pos=(pos+1)%50;
+	while(hashFunc(S[pos].identef)!=i)
+	{	
+		pos=(pos+1)%50;
+		while(S[pos].identef=="\0")
+			pos=(pos+1)%50;
+		if(pos==0)
+			break;
+	}
+	i=pos;
+	if(i==idx)
+	{
+		if(chain[i]==-1)
+		{
+			S[i].identef="\0";
+			S[i].val=-1;
+			upAdjust(i);
+			return;
+		}
+		int c=chain[i];
+		S[i]=S[c];
+		chain[i]=chain[c];
+		S[c].identef="\0";
+		S[c].val=-1;
+		chain[c]=-1;
+		upAdjust(c);
+		return;
+	}
+	int c=i;
+	while(chain[c]!=idx)
+		c=chain[c];
+	chain[c]=chain[idx];
+	S[idx].identef="\0";
+	S[idx].val=-1;
+	chain[idx]=-1;
+	upAdjust(idx);
+}
+void HashTable::upAdjust(int i)
+{
+	int start=i,idx=i;
+	i=(idx+1)%50;
+	//cout<<i<<"\n";
+	while(true)
+	{
+		if(idx==hashFunc(S[i].identef))
+		{
+			S[idx]=S[i];
+			chain[idx]=chain[i];
+			S[i].identef="\0";
+			S[i].val=-1;
+			chain[i]=-1;
+			idx=i;
+		}
+		i=(i+1)%50;
+		if(i==start)
+			break;	
+	}
+}
+int main()
+{
+	HashTable H1,H2;
+	string str,tar;
+	int ch1,ch2,val,col=0;
+	do
+	{
+		cout<<"Enter\n1 For Without Replacement\n2 For With Replacement\n0 To EXIT\n";
+		cin>>ch1;
+		switch(ch1)
+		{
+			case 1:
+			do
+			{
+				cout<<"Enter\n1 To Insert\n2 To Search\n3 To Modify\n4 To Remove\n0 To Prev Menu\n";
+				cin>>ch2;
+				cin.ignore();
+				switch(ch2)
+				{
+					case 1:
+						cout<<"Enter the Identifier\n";
+						getline(cin,str);
+						cout<<"Enter it's value\n";
+						cin>>val;
+						H1.insertWoReplacement(str,val);
+						H1.disp();
+						break;
+					case 2:
+					{
+						cout<<"Enter the Identifier To Search\n";
+						getline(cin,str);
+						int i=H1.searchWoReplacement(str,col);
+						if(i!=-1)
+						{
+							H1.printInfo(i);
+							cout<<"No.of Collisions is/are "<<col<<"\n";
+						}
+						else
+							cout<<"Element not found\n";
+						break;
+					}
+					case 3:
+						cout<<"Enter the Identifier to Modify\n";
+						getline(cin,str);
+						cout<<"Enter new Value\n";
+						cin>>val;
+						H1.modifyWoReplacement(str,val);
+						H1.disp();
+						break;
+					case 4:
+						cout<<"Enter the String to Remove\n";
+						getline(cin,str);
+						H1.delWoReplacement(str);
+						H1.disp();
+						break;
+					case 0:
+						break;
+				}
+			}while(ch2);
+			break;
+			case 2:
+			do
+			{
+				cout<<"Enter\n1 To Insert\n2 To Search\n3 To Modify\n4 To Remove\n0 To Prev Menu\n";
+				cin>>ch2;
+				cin.ignore();
+				switch(ch2)
+				{
+					case 1:
+						cout<<"Enter the Identifier\n";
+						getline(cin,str);
+						cout<<"Enter it's value\n";
+						cin>>val;
+						H2.insertWithReplacement(str,val);
+						H2.disp();
+						break;
+					case 2:
+					{
+						cout<<"Enter the Identifier To Search\n";
+						getline(cin,str);
+						int i=H2.searchWithReplacement(str,col);
+						if(i!=-1)
+						{
+							H2.printInfo(i);
+							cout<<"No.of Collisions is/are "<<col<<"\n";
+						}
+						else
+							cout<<"Element not found\n";
+						break;
+					}
+					case 3:
+						cout<<"Enter the Identifier to Modify\n";
+						getline(cin,str);
+						cout<<"Enter new Value\n";
+						cin>>val;
+						H2.modifyWithReplacement(str,val);
+						H2.disp();
+						break;
+					case 4:
+						cout<<"Enter the String to Remove\n";
+						getline(cin,str);
+						H2.delWithReplacement(str);
+						H2.disp();
+						break;
+					case 0:
+						break;
+				}
+			}while(ch2);
+			break;
+			case 0:
+				cout<<"You have opted to EXIT\n";
+				break;
+			default:
+				cout<<"Invalid Choice\n";
+		}
+	}while(ch1);
+	return 0;
+}
